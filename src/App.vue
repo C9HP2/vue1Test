140fc0a34c7eda18b6a65628b2c6997aeefe4cf8
C9HP2 <sina.dot.hp@gmail.com>
2024-05-10 01:25:17 +0330

    Dynamic Component Label

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id='app'>
+    <h1>To-do List</h1>
+    <ToDoItem label="My ToDo Item"></ToDoItem> 
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoItem
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
