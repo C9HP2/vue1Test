c7c2eb7ac5ce68f7fe2d8fb6657420ce523be26b
C9HP2 <sina.dot.hp@gmail.com>
2024-05-11 18:54:45 +0330

    Dynamic id, for in ToDoItems multi ToDos rendered

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id='app'>
     <h1>To-do List</h1>
-    <ToDoItem label="My ToDo Item"></ToDoItem> 
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <ToDoItem :label="item.label" :done="item.done"></ToDoItem>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { nanoid } from "nanoid";
 import ToDoItem from './components/ToDoItem.vue';
 
 export default {
   name: 'App',
-  components: {
-    ToDoItem
-  },
-}
+  components: {ToDoItem},
+
+  data() {
+    return {
+      ToDoItems: [
+        { label: "Learn Vue", done: false, id: "todo-" + nanoid()},
+        { label: "Create a Vue project with the CLI", done: true,  id: "todo-" + nanoid()},
+        { label: "Have fun", done: true,  id: "todo-" + nanoid()},
+        { label: "Create a to-do list", done: false,  id: "todo-" + nanoid()},
+      ]
+    };
+  }
+};
 </script>
 
 <style>
